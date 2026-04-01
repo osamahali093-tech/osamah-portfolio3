32e5388b60da073412bfb55412207d1bbe308965
osamahali093-tech <osamahali093@gmail.com>
2026-04-02 01:39:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,9 @@ filters.forEach(button => {
     });
   });
 });
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', (e) => {
+  glow.style.left = e.clientX + 'px';
+  glow.style.top = e.clientY + 'px';
+});
